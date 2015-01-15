651c2cd42a5f72e776896ec2c09be03b5110683b
iJoshuaHD <iJoshuaHD@users.noreply.github.com>
2015-01-16 00:39:15 +0800

    spaces and indents :P

--- src/iJoshuaHD/iMCPE/ASR/Database.php
+++ src/iJoshuaHD/iMCPE/ASR/Database.php
@@ -63,10 +63,11 @@ class Database{
 					$port = $this->plugin->getServer()->getPort();
 					$processid = getmypid();
 					$result = $this->db->query("SELECT port FROM asr_logger WHERE port='$port'")->fetch_assoc();
-$current = intval(time());
+					$port = $this->plugin->getServer()->getPort();
+					$current = intval(time());
 					if(!$result){
 						$this->plugin->getLogger()->info(TextFormat::YELLOW ."Adding new processID for PORT: $port ...");
-						$temp = $this->db->query("INSERT INTO asr_logger(port, processid,timestamp)VALUES('$port','$processid','$current')");
+						$temp = $this->db->query("INSERT INTO asr_logger(port, processid, timestamp)VALUES('$port','$processid','$current')");
 						if($temp) $this->plugin->getLogger()->info(TextFormat::GREEN ."SUCCESS!");
 					}else{
 						$this->plugin->getLogger()->info(TextFormat::YELLOW ."Updating new processID for PORT: $port ...");
